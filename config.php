86630a9dc67f51fa42df25fc46373a542356ae92
yura <manko.yuriy@gmail.com>
2018-09-16 22:23:27 +0300

    iss4 Authentication user

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manko
+ * Date: 15.09.2018
+ * Time: 22:06
+ */
+
+
+require_once __DIR__ . '/object/Authentication.php';
+require_once __DIR__ . '/object/User.php';
+require_once __DIR__ . '/object/Lottery.php';
--- object/Authentication.php
+++ object/Authentication.php
@@ -8,7 +8,37 @@
 
 namespace Authentication;
 
+use User\User;
+
 class Authentication
 {
+    private $user;
 
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
+    public function checkAuthentication()
+    {
+        if (!$this->user->getToken()){
+            if($this->authentication())
+                return true;
+            else
+                return false;
+        }
+        else
+            return true;
+    }
+
+    private function authentication()
+    {
+        if (($_POST['username'] == $this->user->getPhone() )
+            && ($_POST['password'] == $this->user->getPassword())){
+                $this->user->generateToken();
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
